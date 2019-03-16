84951abca03f45f23693622f2a9c3876f80fe91f
MrHidalgo <Luckyhidalgo@gmail.com>
2019-03-16 03:59:58 +0200

    What we do page - create stand section - w\ adaptive.

--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -88,4 +88,86 @@ const initSwiper = () => {
       },
     }*/
   });
+
+  const swiperStand = new Swiper('.swiper-container-stand', {
+    // Optional parameters
+    wrapperClass: "swiper-wrapper",
+    slideClass: "swiper-slide",
+    direction: 'horizontal', // 'horizontal' or 'vertical'
+    loop: true,
+    watchOverflow: true,
+    normalizeSlideIndex: true,
+    grabCursor: true,
+    freeMode: false,
+    speed: 500,
+    effect: 'slide', // "slide", "fade", "cube", "coverflow" or "flip"
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    // Disable preloading of all images
+    // preloadImages: false,
+    // Enable lazy loading
+    // lazy: {
+    //   loadPrevNext: true,
+    // },
+
+    // off touch for destop
+    // touchMoveStopPropagation:false,
+    // simulateTouch : false,
+    // allowSwipeToNext: true,
+    // allowSwipeToPrev: true,
+    // allowPageScroll: "auto ",
+
+    initialSlide: 1,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    centeredSlides: true,
+    /*breakpoints: {
+      // when window width is <= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is <= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },*/
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      // renderBullet: function (index, className) {
+      //   return `
+      //     <div class="${className}">
+      //       ${index}
+      //     </div>
+      //   `;
+      // }
+    },
+
+    // Navigation arrows
+    /*navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },*/
+
+    // And if we need scrollbar
+    /*scrollbar: {
+      el: '.swiper-scrollbar',
+    },*/
+
+    /*on: {
+      "slideChange": function () {
+        console.log("slideChange");
+      },
+    }*/
+  });
 };
