a7572af326bb1fdf363049719681c449c9834072
MrHidalgo <Luckyhidalgo@gmail.com>
2019-03-20 10:00:30 +0200

    Apply page - full adaptive.

--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -90,9 +90,6 @@ const initSwiper = () => {
   });
 
   const swiperApply = new Swiper('.swiper-container-apply', {
-    // Optional parameters
-    wrapperClass: "swiper-wrapper",
-    slideClass: "swiper-slide",
     direction: 'vertical', // 'horizontal' or 'vertical'
     loop: true,
     watchOverflow: true,
@@ -101,74 +98,23 @@ const initSwiper = () => {
     freeMode: false,
     speed: 500,
     effect: 'slide', // "slide", "fade", "cube", "coverflow" or "flip"
-    // autoplay: {
-    //   delay: 5000,
-    // },
-    // Disable preloading of all images
-    // preloadImages: false,
-    // Enable lazy loading
-    // lazy: {
-    //   loadPrevNext: true,
-    // },
-
-    // off touch for destop
-    // touchMoveStopPropagation:false,
-    // simulateTouch : false,
-    // allowSwipeToNext: true,
-    // allowSwipeToPrev: true,
-    // allowPageScroll: "auto ",
-
     initialSlide: 1,
     slidesPerView: 'auto',
     spaceBetween: 47,
     centeredSlides: true,
-    /*breakpoints: {
-      // when window width is <= 320px
-      320: {
+    breakpoints: {
+      // when window width is <= 767px
+      767: {
+        direction: 'horizontal',
         slidesPerView: 1,
-        spaceBetween: 10
-      },
-      // when window width is <= 480px
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is <= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 30
+        spaceBetween: 0,
+        centeredSlides: false
       }
-    },*/
-
-    // If we need pagination
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-      // renderBullet: function (index, className) {
-      //   return `
-      //     <div class="${className}">
-      //       ${index}
-      //     </div>
-      //   `;
-      // }
-    },
-
-    // Navigation arrows
-    /*navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },*/
-
-    // And if we need scrollbar
-    /*scrollbar: {
-      el: '.swiper-scrollbar',
-    },*/
-
-    /*on: {
-      "slideChange": function () {
-        console.log("slideChange");
-      },
-    }*/
+    }
   });
 
   const swiperStand = new Swiper('.swiper-container-stand', {
